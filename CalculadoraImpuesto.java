301ad70546b575d9ee81b56228aa38e9939a8c26
Cristian Gallegos <jacob.gallegos.c@gmail.com>
2023-07-12 17:52:48 -0600

    minor changes

---
+++ CalculadoraImpuesto.java
@@ -0,0 +1,12 @@
+public class CalculadoraImpuesto {
+  private double totalImpuesto;
+
+  public void registra(Tributacion t) {
+    double valor = t.getValorImpuesto();
+    this.totalImpuesto += valor;
+  }
+
+  public double getTotalImpuesto() {
+    return totalImpuesto;
+  }
+}
